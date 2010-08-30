cc499fcb0a24426cf1ae9d2717abe616583df95e
Thomas Kadauke <tkadauke@imedo.de>
2010-08-31 01:04:56 +0200

    name fix

--- rails_generators/tiny_account/tiny_account_generator.rb
+++ rails_generators/tiny_account/tiny_account_generator.rb
@@ -24,7 +24,7 @@ class TinyAccountGenerator < Rails::Generator::Base
       m.file "user_accounts_controller.rb", 'app/controllers/user_accounts_controller.rb'
       m.file "admin_accounts_controller.rb", 'app/controllers/admin/accounts_controller.rb'
       m.file "accounts_controller_test.rb", 'test/functional/accounts_controller_test.rb'
-      m.file "user_accounts_controller_test.rb", 'test/functional/user_accounst_controller_test.rb'
+      m.file "user_accounts_controller_test.rb", 'test/functional/user_accounts_controller_test.rb'
       m.file "admin_accounts_controller_test.rb", 'test/functional/admin/accounts_controller_test.rb'
       
       # Locales
